be7fb96b43c77e7a79220dbcc1ee03e414500d18
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-07-28 23:47:00 +0500

    Skills Updated

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -59,7 +59,7 @@ const Skills = () => {
               </span>
             </div>
             <div className="overflow-x-hidden">
-              <p className="text-sm uppercase font-medium">Next js</p>
+              <p className="text-sm uppercase font-medium">Node js</p>
               <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
                 <motion.span
                   initial={{ x: "-100%", opacity: 0 }}
@@ -81,6 +81,19 @@ const Skills = () => {
                   className="w-[85%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
                 >
                   <span className="absolute -top-7 right-0">85%</span>
+                </motion.span>
+              </span>
+            </div>
+            <div className="overflow-x-hidden">
+              <p className="text-sm uppercase font-medium">C++</p>
+              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+                <motion.span
+                  initial={{ x: "-100%", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.5 }}
+                  className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                >
+                  <span className="absolute -top-7 right-0">90%</span>
                 </motion.span>
               </span>
             </div>
@@ -184,6 +197,19 @@ const Skills = () => {
                   className="w-[70%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
                 >
                   <span className="absolute -top-7 right-0">70%</span>
+                </motion.span>
+              </span>
+            </div>
+            <div className="overflow-x-hidden">
+              <p className="text-sm uppercase font-medium">Email js</p>
+              <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+                <motion.span
+                  initial={{ x: "-100%", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.5 }}
+                  className="w-[80%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+                >
+                  <span className="absolute -top-7 right-0">80%</span>
                 </motion.span>
               </span>
             </div>
