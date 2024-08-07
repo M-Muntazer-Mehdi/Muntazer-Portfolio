7d927bf2114b7823a8adeefb113c5a21c27c4ec4
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-08-07 05:02:11 +0500

    Remove Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Banner from "./components/banner/Banner";
 import Contact from "./components/contact/Contact";
 import Features from "./components/features/Features";
-import Footer from "./components/footer/Footer";
 import FooterBottom from "./components/footer/FooterBottom";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
@@ -21,7 +20,6 @@ function App() {
         <Projects />
         <Resume />
         <Contact />
-        <Footer />
         <FooterBottom />
       </div>
   </div>
